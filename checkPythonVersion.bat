491c8d0c11a308a45e905fb7eec3475d7bc4f700
mariiasharkovskaya <mariasharkovskaya2003@gmail.com>
2024-07-24 01:50:58 +0300

    Add freeOsc compilation source file into an executable file

--- checkPythonVersion.bat
+++ checkPythonVersion.bat
@@ -6,5 +6,7 @@ for /f "tokens=2 delims=." %%a in ('python --version') do (
     set MINOR=%%a
 )
 
-g++ forcedOsc.cpp -o mygif -I C:/msys64/mingw64/include -L/msys64/mingw64/lib -lgsl -lgslcblas -I "C:/Program Files/Python3!MINOR!/include/" -I C:/Users/Mary/AppData/Roaming/Python/Python3!MINOR!/site-packages/numpy/core/include/ -I "C:/opencv/build/include" -L "C:/opencv/build/" -L "C:/Program Files/Python3!MINOR!/libs" -lpython3!MINOR!
-mygif.exe
+g++ forcedOsc.cpp -o forcedOsc -I C:/msys64/mingw64/include -L/msys64/mingw64/lib -lgsl -lgslcblas -I "C:/Program Files/Python3!MINOR!/include/" -I C:/Users/Mary/AppData/Roaming/Python/Python3!MINOR!/site-packages/numpy/core/include/ -I "C:/opencv/build/include" -L "C:/opencv/build/" -L "C:/Program Files/Python3!MINOR!/libs" -lpython3!MINOR!
+forcedOsc.exe
+g++ freeOsc.cpp -o freeOsc -I C:/msys64/mingw64/include -L/msys64/mingw64/lib -lgsl -lgslcblas -I "C:/Program Files/Python3!MINOR!/include/" -I C:/Users/Mary/AppData/Roaming/Python/Python3!MINOR!/site-packages/numpy/core/include/ -I "C:/opencv/build/include" -L "C:/opencv/build/" -L "C:/Program Files/Python3!MINOR!/libs" -lpython3!MINOR!
+freeOsc.exe
